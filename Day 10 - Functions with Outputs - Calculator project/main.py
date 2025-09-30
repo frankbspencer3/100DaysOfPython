691b0533e011f90e9fcd54d3bdeb97bc3d12c243
frankbspencer3 <frank.b.spencer.3@gmail.com>
2025-09-30 14:10:25 -0400

    Day 10 - Calculator v2

--- Day 10 - Functions with Outputs - Calculator project/main.py
+++ Day 10 - Functions with Outputs - Calculator project/main.py
@@ -1,5 +1,4 @@
 from art import logo
-
 print(logo)
 
 def add(n1, n2):
@@ -16,10 +15,32 @@ def divide(n1, n2):
 
 math_dict = {"+": add, "-": sub, "*": multiply, "/": divide,}
 
-math = True
-while math:
-    number1 = int(input("Whats the first number bruv: "))
+# math = True
+# while math:
+#     number1 = int(input("Whats the first number bruv: "))
+#
+#     continuing = True
+#     while continuing:
+#
+#         for symbol in math_dict:
+#             print(symbol)
+#         operator = input("What operator do you want m8? ")
+#         number2 = int(input("Whats the next number bruv: "))
+#
+#         result = math_dict[operator](number1, number2)
+#         print(result)
+#
+#         cont = input(f"Do you want to continue with {result} or restart? Y/N").lower()
+#         if cont == "y":
+#             continuing = True
+#             number1 = result
+#         elif cont == "n":
+#             continuing = False
+#         else:
+#             break
 
+def calculator():
+    number1 = int(input("Whats the first number bruv: "))
     continuing = True
     while continuing:
 
@@ -37,5 +58,8 @@ while math:
             number1 = result
         elif cont == "n":
             continuing = False
+            calculator()
         else:
             break
+
+calculator()
