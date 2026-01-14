1aa6c80e397f5dbbfc39a9f7cc33f290007c8845
frankbspencer3 <frank.b.spencer.3@gmail.com>
2026-01-14 10:26:38 +0000

    Day 3 progress

--- Day 17v2 - OOP Supplemental learning/OOPDay3.py
+++ Day 17v2 - OOP Supplemental learning/OOPDay3.py
@@ -10,14 +10,16 @@ class Book:
     
 class EBook(Book):
 
-    def __init__(self, file_size):
-        Book.__init__(self)
+    def __init__(self,title, author, year, file_size):
+        super().__init__(title, author, year)
         self.file_size = file_size
 
     def get_summary(self):
-        return super().get_summary()
+        return f"{super().get_summary()}, File Size: {self.file_size}"
 
 Book1 = Book("stars", "Lay", "2026")
 
-EBook1 = EBook.Book1("10256")
-print(EBook)
+EBook1 = EBook("stars", "Spencer", "2026", "51255")
+
+print(Book1.get_summary())
+print(EBook1.get_summary())
